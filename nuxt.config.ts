6828778115b280944cbc04a985a5df50b3e0648a
rmnvnc <romanvanco23@gmail.com>
2025-11-25 10:25:52 +0100

    nuxt-init - add tailwind css (important for NUXT UI)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/hints', '@nuxt/ui']
+  modules: ['@nuxt/eslint', '@nuxt/hints', '@nuxt/ui'],
+  css: ['~/assets/css/main.css']
 })
